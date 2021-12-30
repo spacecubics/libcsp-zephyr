66c994a32d28fcf45da9224d4a7d3072be9f81f5
Yasushi SHOJI <yashi@spacecubics.com>
2021-12-30 23:27:09 +0900

    initial

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2022 Space Cubics, LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <sys/printk.h>
+
+void main(void)
+{
+	printk("Hello World! %s\n", CONFIG_BOARD);
+}
